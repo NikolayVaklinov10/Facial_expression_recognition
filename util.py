66855b798d76df5bf479a71874255e50683050d1
Nick <nikolayvaklinov@yahoo.com>
2019-09-25 20:17:10 +0100

    adding more code in util

--- util.py
+++ util.py
@@ -2,7 +2,6 @@ from __future__ import print_function, division
 from builtins import range
 # Note: you may need to update your version of future
 # sudo pip install -U future
-from random import shuffle
 
 import numpy as np
 import pandas as pd
@@ -33,7 +32,7 @@ def softmax(A):
 
 
 def sigmoid_cost(T, Y):
-    return - (T*np.log(Y) + (1-T)*np.log(1-Y)).sum()
+    return -(T*np.log(Y) + (1-T)*np.log(1-Y)).sum()
 
 
 def cost(T, Y):
@@ -41,8 +40,8 @@ def cost(T, Y):
 
 
 def cost2(T, Y):
-    #  same as cost(), just uses the target to index Y
-    #  instead of multiplying by a large indicator matrix with mostly 0s
+    # same as cost(), just uses the targets to index Y
+    # instead of multiplying by a large indicator matrix with mostly 0s
     N = len(T)
     return -np.log(Y[np.arange(N), T]).mean()
 
@@ -61,7 +60,7 @@ def y2indicator(y):
 
 
 def getData(balance_ones=True):
-    #  images are 48x48 = 2304 size vectors
+    # images are 48x48 = 2304 size vectors
     Y = []
     X = []
     first = True
@@ -73,17 +72,17 @@ def getData(balance_ones=True):
             Y.append(int(row[0]))
             X.append([int(p) for p in row[1].split()])
 
-        X, Y = np.array(X) / 255.0, np.array(Y)
+    X, Y = np.array(X) / 255.0, np.array(Y)
 
-        if balance_ones:
-            # balance the 1 class
-            X0, Y0 = X[Y!=1, :], Y[Y!=1]
-            X1 = X[Y==1, :]
-            X1 = np.repeat(X1, 9, axis=0)
-            X = np.vstack([X0, X1])
-            Y = np.concatenate((Y0, [1]*len(X1)))
+    if balance_ones:
+        # balance the 1 class
+        X0, Y0 = X[Y!=1, :], Y[Y!=1]
+        X1 = X[Y==1, :]
+        X1 = np.repeat(X1, 9, axis=0)
+        X = np.vstack([X0, X1])
+        Y = np.concatenate((Y0, [1]*len(X1)))
 
-        return X, Y
+    return X, Y
 
 
 def getImageData():
@@ -126,7 +125,6 @@ def crossValidation(model, X, Y, K=5):
         errors.append(err)
     print("errors:", errors)
     return np.mean(errors)
-
 
 
 
